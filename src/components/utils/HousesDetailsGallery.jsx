1b47b06529624ba444d5af36c6a2841eaaf6e114
olivermares <olivermares@hotmail.com>
2023-08-04 22:03:08 +0200

    test2

---
+++ src/components/utils/HousesDetailsGallery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HousesDetailsGallery( data ) {
+  //const dataAux= data.map((element)=> {...element, element.image: })
+  console.log(data)
+  return (
+      <div className="house-card">
+          <h2>fñjkbak</h2>
+        
+      </div>
+  );
+}
