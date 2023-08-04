4c66c036a8940485532faf0d389b44943408a2ec
Marta Escribano <martaeg309@gmail.com>
2023-08-04 18:36:01 +0200

    testing styles homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import "../../styles/utils/_bng-home.utils.scss"
+
+export default function HomePage() {
+  return (
+    <div className="homepage" >
+      <h2>GAMES OF THRONES</h2>
+    </div>
+      
+  );
+}
+
--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -1,15 +1,17 @@
 import { Routes as RoutesDom } from "react-router-dom";
 import { Route } from "react-router-dom";
 
-import HomePage from "./HomePagePepe/HomePage";
+
 import HousesPage from "./HousesPage/HousesPage";
 import CharactersPage from "./CharactersPage/CharactersPage";
-import ChronologyPage from "./ ChronologyPage / ChronologyPage";
+import ChronologyPage from "./ChronologyPage/ChronologyPage";
+import HomePage from "./HomePage/HomePage";
+
 
 export default function Routes() {
   return (
     <RoutesDom>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage/>} />
       <Route path="/houses" element={<HousesPage />} />
       <Route path="/characters" element={<CharactersPage />} />
       <Route path="/chronology" element={<ChronologyPage />} />
