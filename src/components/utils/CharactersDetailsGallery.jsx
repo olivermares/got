8382d579eaf3d31504a18530bc463bfd4a275112
olivermares <olivermares@hotmail.com>
2023-08-05 23:02:25 +0200

    crear elememts css y un poco de limpieza

--- src/components/utils/CharactersDetailsGallery.jsx
+++ src/components/utils/CharactersDetailsGallery.jsx
@@ -1,45 +1,47 @@
 import React from "react";
 
-export default function CharactersDetailsGallery({ data , img}) {
-console.log("http://localhost:3000" + img)
-console.log("http://localhost:3000" + data.image)
+export default function CharactersDetailsGallery({ data, img }) {
+  console.log("http://localhost:3000" + img);
+  console.log("http://localhost:3000" + data.image);
   return (
     <div className="details">
       <img src={"http://localhost:3000" + data.image} alt={data.name} />
-      <h2>{data.name}</h2>
+      <h2 className="e-h2">{data.name}</h2>
       <div className="details-cards">
         <div className="details-card-detail">
-          <h3>CASA</h3>
-          <img src={"http://localhost:3000" + img} alt={"escudo"}/> 
+          <h3 className="e-h3">CASA</h3>
+          <img src={"http://localhost:3000" + img} alt={"escudo"} />
         </div>
         <div className="details-card-detail">
-          <h3>ALIANZAS</h3>
+          <h3 className="e-h3">ALIANZAS</h3>
           {data.alliances.map((item) => (
-            <p key={item}>{item}</p>
+            <p className="e-p" key={item}>
+              {item}
+            </p>
           ))}
         </div>
         <div className="details-card-detail">
-          <h3>APARICIONES</h3>
+          <h3 className="e-h3">APARICIONES</h3>
           {data.episodes.map((item) => (
-            <p key={item}>{item}</p>
+            <p className="e-p" key={item}>{item}</p>
           ))}
         </div>
         <div className="details-card-detail">
-          <h3>PADRES</h3>
+          <h3 className="e-h3">PADRES</h3>
           {data.parents.map((item) => (
-            <p key={item}>{item}</p>
+            <p className="e-p" key={item}>{item}</p>
           ))}
         </div>
         <div className="details-card-detail">
-          <h3>DESCENDIENTES</h3>
+          <h3 className="e-h3">DESCENDIENTES</h3>
           {data.siblings.map((item) => (
-            <p key={item}>{item}</p>
+            <p className="e-p" key={item}>{item}</p>
           ))}
         </div>
         <div className="details-card-detail">
-          <h3>TITULOS</h3>
+          <h3 className="e-h3">TITULOS</h3>
           {data.titles.map((item) => (
-            <p key={item}>{item}</p>
+            <p className="e-p" key={item}>{item}</p>
           ))}
         </div>
       </div>
