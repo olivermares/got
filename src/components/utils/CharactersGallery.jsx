0ec2bb797cc870fe14da59dd3a50a4c8d13b823b
Marta Escribano <martaeg309@gmail.com>
2023-08-06 20:45:30 +0200

    scroll

--- src/components/utils/CharactersGallery.jsx
+++ src/components/utils/CharactersGallery.jsx
@@ -3,18 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function CharactersGallery({data}) {
   return (
-    <div className="row">
-              {data.map((item) => (          
-          <div key={item.id} className="col-6 col-lg-2">
-          <Link to={`/characters/${item.id}`}>
-            <img className='img hideDiv margin-top' src={"http://localhost:3000" + item.image} alt={item.name} />
-            <div className='hide alignCenter'>
-              <h4 className='e-h4 shadow'>{item.name}</h4>
+    <>
+      <div className="list-group" id="list">
+          {data.map((item) => (          
+          <a className="list-group-item list-group-item-action" href={item.id}>{item}</a>
+          ))} 
+      </div>
+      
+          <div data-bs-spy="scroll" data-bs-target="#list" data-bs-offset="0" className= "scrollspy" tabindex="0">
+          <div className="row">
+          {data-bs-target.map((item) => (
+            <div key={item.id} className="col-6 col-lg-2">
+            <Link to={`/characters/${item.id}`}>
+              <img className='img hideDiv margin-top' src={"http://localhost:3000" + item.image} alt={item.name} />
+              <div className='hide alignCenter'>
+                <h4 className='e-h4 shadow'>{item.name}</h4>
+              </div>
+              </Link>
             </div>
-            </Link>
-          </div>
-         
-        ))} 
-    </div>
+            ))}
+            <p>{item.id}</p>
+        </div>
+
+      
+      </div>
+    </>
   )
 }
+
