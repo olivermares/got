a78a59cf970e496de209a798a81928d479d9d3c6
Marta Escribano <martaeg309@gmail.com>
2023-08-07 22:04:24 +0200

    last version

--- src/components/utils/CharactersGallery.jsx
+++ src/components/utils/CharactersGallery.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import SimpleBar from 'simplebar-react';
+import SimpleBar from "simplebar-react";
 
-export default function CharactersGallery({data}) {
+export default function CharactersGallery({ data }) {
   return (
-    <div className='container'>
-    <SimpleBar forceVisible="y" autoHide="false" style={{ maxHeight: 500 }}>
-    <div className="row container">
-              {data.map((item) => (          
-          <div key={item.id} className="col-6 col-lg-2">
-          <Link to={`/characters/${item.id}`}>
-            <img className='img hideDiv margin-top' src={"http://localhost:3000" + item.image} alt={item.name} />
-            <div className='hide alignCenter'>
-              <h4 className='e-h4 shadow'>{item.name}</h4>
+    <div className="container">
+      <SimpleBar forceVisible="y" autoHide="false" style={{ maxHeight: 500 }}>
+        <div className="row container">
+          {data.map((item) => (
+            <div key={item.id} className="col-6 col-lg-2">
+              <Link to={`/characters/${item.id}`} >
+              <div className="card">
+                <img
+                  className="img hideDiv "
+                  src={"http://localhost:3000" + item.image}
+                  alt={item.name}
+                />
+                <div className="hide alignCenter">
+                  <h4 className="e-h4 shadow">{item.name}</h4>
+                </div>
+                </div>
+              </Link>
             </div>
-            </Link>
-          </div>
-         
-        ))} 
+          ))}
+        </div>
+      </SimpleBar>
     </div>
-    </SimpleBar>
-    </div>
-  )
+  );
 }
--- src/components/utils/HousesGallery.jsx
+++ src/components/utils/HousesGallery.jsx
@@ -4,20 +4,27 @@ import SimpleBar from "simplebar-react";
 
 export default function HousesGallery({ data }) {
   return (
-    <div className='container'>
-    <SimpleBar forceVisible="y" autoHide="false" style={{ maxHeight: 500 }}>
-      <div className="row container">
-        {data.map((item) => (          
-          <div key={item.id} className="col-6 col-lg-2">
-          <Link to={`/houses/${item.id}`}>
-            <img className='img margin-top' src={"http://localhost:3000" + item.image} alt={item.name} />
-            <h4 className="e-h4 alignCenter margin-top margin-bottom">{item.name}</h4>
-            </Link>
-          </div>
-         
-        ))}       
-      </div>
+    <div className="container">
+      <SimpleBar forceVisible="y" autoHide="false" style={{ maxHeight: 500 }}>
+        <div className="row container">
+          {data.map((item) => (
+            <div key={item.id} className="col-6 col-lg-2">
+              <Link to={`/houses/${item.id}`}>
+                <div className="card">
+                  <img
+                    className="img"
+                    src={"http://localhost:3000" + item.image}
+                    alt={item.name}
+                  />
+                  <h4 className="e-h4 alignCenter">
+                    {item.name}
+                  </h4>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </SimpleBar>
-      </div>
+    </div>
   );
 }
